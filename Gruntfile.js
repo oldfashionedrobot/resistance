b6f28c0f8716b0ecd7539d63b03bbae5fef3e59c
oldfashionedrobot <sanjeev@oldfashionedrobot.com>
2014-11-24 21:17:42 -0500

    added grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,13 +28,30 @@ module.exports = function(grunt) {
     },
     jshint: {
       all: ['Gruntfile.js', 'app/js/src/**/*.js']
+    },
+    watch: {
+      styles: {
+        files: 'app/css/src/**/*.scss',
+        tasks: 'sass'
+      },
+      scripts: {
+        files: 'app/js/src/**/*.js',
+        tasks: 'uglify' 
+      },
+      configFiles: {
+        files: 'Gruntfile.js'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-wiredep');
 
   grunt.registerTask('default', ['uglify', 'sass']);
+  grunt.registerTask('all', ['wiredep', 'uglify', 'sass']);
+  grunt.registerTask('lint', ['jshint']);
+  grunt.registerTask('start_watch', ['all', 'watch']);
 };
--- app/js/src/main.js
+++ app/js/src/main.js
@@ -1,1 +1,7 @@
-var APP = angular.module( "myApp", [ "firebase", "route", "Restangular" ] );
+var APP = angular.module( "resistance", [ "firebase", "ngRoute", "restangular" ] );
+
+APP.controller( 'MyController', [ '$scope', '$firebase', 'Restangular',
+  function( $scope, $firebase, Restangular ) {
+   // fsdfs 
+  }
+]);
