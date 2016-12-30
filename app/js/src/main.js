4c6e9936abf3161cf382399fe15da2c5f2ab0e12
Sanjeev Nayak <snayak@springbot.com>
2016-12-30 11:30:38 -0500

    clear

--- app/js/src/main.js
+++
@@ -1,7 +0,0 @@
-var APP = angular.module( "resistance", [ "firebase", "ngRoute", "restangular" ] );
-
-APP.controller( 'MyController', [ '$scope', '$firebase', 'Restangular',
-  function( $scope, $firebase, Restangular ) {
-   // fsdfs 
-  }
-]);
